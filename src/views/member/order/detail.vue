56cad98fdc368a68ca1c9eb097f00c5df4f5a3a0
A_ye <1464102511@qq.com>
2022-05-09 17:04:33 +0800

    订单详情头部展示-steps步骤条封装

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -1,16 +1,37 @@
 <template>
-  <div class="member-order">
-      MemberDetail
-
-    </div>
+  <div class="member-order-detail" v-if="order">
+    <!-- 头部 -->
+    <OrderDetailAction :order="order" />
+    <!-- 进度 -->
+    <DetailStep :order="order" />
+    <!-- 物流 -->
+    <!-- 信息 -->
+  </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import OrderDetailAction from './components/detail-action'
+import DetailStep from './components/detail-step'
+import { findOrderDetail } from '@/api/order'
+import { useRoute } from 'vue-router'
 export default {
-  name: 'MemberDetail'
+  name: 'MemberDetail',
+  components: { OrderDetailAction, DetailStep },
+  setup () {
+    const route = useRoute()
+    const order = ref(null)
+    findOrderDetail(route.params.id).then((data) => {
+      order.value = data.result
+    })
+    return { order }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.member-order-detail {
+  background: #fff;
+  height: 100%;
+}
 </style>
